a578ff203bad7d1bcedc937f3af4f231938ebc3f
ISpity <ispity@epitech.eu>
2026-03-13 12:44:55 +0100

    MAINTEST

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <string>
-
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
+    std::cout << "Hello, world!aqeqeqeqeqe:q" << std::endl;
     return 0;
 }
